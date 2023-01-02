a7f3ff352fff1afeb0b9f9a3f06f315b2533ffd9
JaZahn <jcleveng@fas.harvard.edu>
2023-01-02 14:38:53 -0500

    adding a naive build workflow

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 docker build -t dining-api .
 docker tag dining-api artifactory.huit.harvard.edu/aais-docker-local/dining-api:latest
-docker login artifactory.huit.harvard.edu
+
+if [[ -z "${ARTIFACTORY_USER}" && -z "${ARTIFACTORY_PWD}" ]]; then
+    docker login artifactory.huit.harvard.edu --username "${ARTIFACTORY_USER}" --password "${ARTIFACTORY_PWD}"
+else
+    docker login artifactory.huit.harvard.edu
+fi
+
 docker push artifactory.huit.harvard.edu/aais-docker-local/dining-api:latest
