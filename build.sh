1908f5d485affbf3d7ac5bbfbe9618a65e829fb0
JaZahn <jcleveng@fas.harvard.edu>
2023-01-02 15:18:54 -0500

    adding some debug

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ docker build -t dining-api .
 docker tag dining-api artifactory.huit.harvard.edu/aais-docker-local/dining-api:latest
 
 if [[ -z "${ARTIFACTORY_USER}" && -z "${ARTIFACTORY_PWD}" ]]; then
+    echo "has the env vars"
     docker login artifactory.huit.harvard.edu --username "${ARTIFACTORY_USER}" --password "${ARTIFACTORY_PWD}"
 else
+    echo "no env vars?"
     docker login artifactory.huit.harvard.edu
+    env
 fi
 
 docker push artifactory.huit.harvard.edu/aais-docker-local/dining-api:latest
