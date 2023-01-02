3459210391c6af32c3364b1c2e86940994b8ddfd
JaZahn <jcleveng@fas.harvard.edu>
2023-01-02 15:25:17 -0500

    fixing bash logic

--- build.sh
+++ build.sh
@@ -3,13 +3,12 @@
 docker build -t dining-api .
 docker tag dining-api artifactory.huit.harvard.edu/aais-docker-local/dining-api:latest
 
-if [[ -z "${ARTIFACTORY_USER}" && -z "${ARTIFACTORY_PWD}" ]]; then
-    echo "has the env vars"
-    docker login artifactory.huit.harvard.edu --username "${ARTIFACTORY_USER}" --password "${ARTIFACTORY_PWD}"
-else
+if [[ -z "${ARTIFACTORY_USER}" || -z "${ARTIFACTORY_PWD}" ]]; then
     echo "no env vars?"
     docker login artifactory.huit.harvard.edu
-    env
+else
+    echo "has the env vars"
+    docker login artifactory.huit.harvard.edu --username "${ARTIFACTORY_USER}" --password "${ARTIFACTORY_PWD}"
 fi
 
 docker push artifactory.huit.harvard.edu/aais-docker-local/dining-api:latest
